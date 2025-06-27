677d5ac32c885dc56d1ea5c83ff9baa9e10e22e8
Said Magomedov <saidmagomedov037@gmail.com>
2025-06-27 22:16:07 +0500

    Созданы все необходимые модели

---
+++ users/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    pfp = models.ImageField(
+        upload_to="users/pfps",
+        blank=True,
+        null=True,
+        verbose_name="Аватарка",
+        help_text="Выберите аватарку",
+    )
+    phone_number = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Номер телефона",
+        help_text="Укажите номер телефона",
+    )
+    city = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="Город",
+        help_text="Укажите город",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
