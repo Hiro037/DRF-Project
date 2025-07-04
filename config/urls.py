f0d616947f6c0cf643402ea1a636730f36a7b72d
Said Magomedov <saidmagomedov037@gmail.com>
2025-07-04 22:57:22 +0500

    Исправление ошибок

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ from config import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('materials/', include('materials.urls', namespace='materials')),
-    # path('users/', include('users.urls', namespace='users'))
+    path('users/', include('users.urls', namespace='users'))
 ]
 
 if settings.DEBUG:
